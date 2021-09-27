e9e0e48f8207934c7bd2680e2cdd395bf88d5d92
Dishant <dishant0170@gmail.com>
2021-09-27 09:37:06 +0530

    small changes

--- src/components/Authentication/login/index.jsx
+++ src/components/Authentication/login/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, withRouter} from 'react-router-dom'
+import { Link, withRouter } from "react-router-dom";
 import { Input } from "reactstrap";
 import { connect } from "react-redux";
 import { compose } from "redux";
@@ -13,49 +13,71 @@ import google from "../../../assets/images/socialIcon/google-plus@3x.png";
 const { login } = AuthActions;
 
 const Login = (props) => {
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
     const data = {
-      accessToken:'accessToken'
-    }
-    props.login(data)
-    props.history.push('/')
-  }
+      accessToken: "accessToken",
+    };
+    props.login(data);
+    props.history.push("/");
+  };
   return (
-    <div className="form-wrapper text-center">
-       <h2 className="white-text text-uppercase">Login</h2>
-      <div className="row  mt-30">
-        <div className="col-12 pa-0">
-          <Input type="email" name="email" id="email" placeholder="Email" />
-          <img alt="email" className="input-icon" src={EmailIcon} />
+    <div className="form-container">
+      <div className="form-wrapper text-center">
+        <h2 className="white-text text-uppercase">Login</h2>
+        <form onSubmit={handleLogin}>
+          <div className="row  mt-30">
+            <div className="col-12 pa-0">
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="email"
+                placeholder="Email"
+                required
+              />
+              <img alt="email" className="input-icon" src={EmailIcon} />
+            </div>
+            <div className="col-12 mt-24 pa-0">
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                required
+              />
+              <img alt="password" className="input-icon" src={PasswordIcon} />
+            </div>
+            <div
+              className="col-12 fs-14 green-text text-right pa-0 mt-8 cursor-pointer"
+              onClick={() => props.history.push("/forgotPassword")}
+            >
+              Forgot Password?
+            </div>
+          </div>
+          <div className="mt-48">
+            <Button
+              className="bg-black"
+              borderColor="#76c047"
+              label={"Login"}
+              type="submit"
+              style={{ height: "35px", width: "180px" }}
+            />
+          </div>
+        </form>
+        <div className="white-text mt-24 fs-12">OR</div>
+        <div className="justify-content-center  d-flex mt-24">
+          <img alt="facebook" className="socila-media-icon" src={facebook} />
+          <img alt="gmail" className="socila-media-icon ml-48" src={google} />
         </div>
-        <div className="col-12 mt-24 pa-0">
-          <Input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
-          />
-          <img alt="password" className="input-icon" src={PasswordIcon} />
+        <div className="fs-12 white-text mt-16  ">
+          Don't have account?{" "}
+          <Link to="/register" className="green-text ml-1" href="#">
+            Register
+          </Link>
         </div>
-        <div className="col-12 fs-14 green-text text-right pa-0 mt-8 cursor-pointer" onClick={() => props.history.push('/forgotPassword')}>
-          Forgot Password?
-        </div>
-      </div>
-      <div className="mt-48">
-        <Button className="bg-black" borderColor="#76c047" onClick={handleLogin} label={"Login"} style={{ height: "35px", width: "180px" }} />
-      </div>
-      <div className="white-text mt-24 fs-12">OR</div>
-      <div className="justify-content-center  d-flex mt-24">
-        <img alt="facebook" className="socila-media-icon" src={facebook} />
-        <img alt="gmail" className="socila-media-icon ml-48" src={google} />
-      </div>
-      <div className="fs-12 white-text mt-16  ">
-        Don't have account?{" "}
-        <Link to="/register" className="green-text ml-1" href="#">
-          Register
-        </Link>
       </div>
     </div>
   );
 };
-export default compose(withRouter,connect(null, { login }))(Login);
+export default compose(withRouter, connect(null, { login }))(Login);
